51153cc2ead9adfa644e4f927395a4c6a872de00
jimmy <jimmy@dwp.co.id>
2025-01-01 15:02:45 +0700

    feat(wedding-invitation): update image sources and enhance layout for improved presentation

--- src/components/ui/background-image-transition-custom.jsx
+++ src/components/ui/background-image-transition-custom.jsx
@@ -1,11 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 
-export const BackgroundImageTransition = ({
-  className,
-  interval = 3000,
-  images,
-}) => {
+export const BackgroundImageTransition = ({ interval = 3000, images }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
@@ -17,23 +13,23 @@ export const BackgroundImageTransition = ({
   }, [interval, images.length]);
 
   return (
-    <div className="relative min-h-svh isolate overflow-hidden">
+    <>
       {images.map((image, index) => (
         <img
           src={image}
           key={index}
           alt=""
-          className="absolute inset-0 size-full object-cover md:object-scale-down"
+          className="absolute inset-0 size-full object-cover"
           style={{
             transition: `transform ${interval}ms ease-out, opacity ${interval}ms`,
             transform:
               index === currentImageIndex ? `scale(1.2,1.2)` : `scale(1.3,1.3)`,
-            opacity: index === currentImageIndex ? 1 : 0,
+            opacity: index === currentImageIndex ? 0.8 : 0,
             filter: `grayscale(1%)`,
-            zIndex: index === currentImageIndex ? 20 : 10,
+            zIndex: index === currentImageIndex ? -1 : -2,
           }}
         />
       ))}
-    </div>
+    </>
   );
 };
--- src/pages/WeddingInvitationPage.jsx
+++ src/pages/WeddingInvitationPage.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
 import { BackgroundImageTransition } from '@/components/ui/background-image-transition-custom';
-import image1 from '../assets/hero-img1.jpg';
-import image2 from '../assets/hero-img2.jpg';
-import image3 from '../assets/hero-img3.jpg';
-import image4 from '../assets/hero-img4.jpg';
-import image5 from '../assets/hero-img5.jpg';
+import image1 from '../assets/test-1.jpeg';
+import image2 from '../assets/test-2.jpeg';
+import image3 from '../assets/test-3.jpeg';
+import image4 from '../assets/test-4.jpeg';
+import image5 from '../assets/test-5.jpeg';
 import tulips from '../assets/tulip.svg';
 import Preloader from '@/components/ui/preloader-custom';
 
@@ -35,24 +35,37 @@ export default function WeddingInvitationPage() {
 
   return (
     <>
-      <BackgroundImageTransition interval={7000} images={images} />
-      <div className="absolute inset-0 z-20 bg-cyan-950/60 flex items-center justify-center">
-        <div className="flex justify-start space-x-4">
-          <div>
-            <img className="w-40 h-40" src={tulips} alt="tulip" />
-          </div>
-          <div>
-            <h2 className="text-2xl text-center text-white">SAVE THE DATE!</h2>
-            <h1 className="text-8xl text-center text-white tracking-wider">
-              Inka
-              <span className="mx-2"></span>
-              <label className="text-5xl">&amp;</label>
-              <span className="mx-2"></span>
-              Jimmy
-            </h1>
-          </div>
-          <div>
-            <img className="w-40 h-40" src={tulips} alt="tulip" />
+      <div className="bg-cyan-950">
+        <div className="relative w-screen h-screen isolate overflow-hidden">
+          <BackgroundImageTransition interval={7000} images={images} />
+          <div className="w-screen h-screen font-playfair absolute bg-[#F9F2EB]/30">
+            <div className="mx-auto max-w-2xl py-64">
+              <div className="text-center">
+                <h2 className="text-xl font-rubik py-3">SAVE THE DATE</h2>
+                <h1 className="text-balance text-6xl tracking-tight text-white sm:text-8xl">
+                  <span className="block sm:inline tracking-wider font-sacramento">
+                    Inka
+                    <span className="text-5xl"> &amp; </span>
+                    Jimmy
+                  </span>
+                </h1>
+                <hr className="w-20 mx-auto border-2 border-white my-7" />
+                <h3 className="text-xl font-rubik py-3">
+                  Saturday, 16th Aug 2025
+                </h3>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                  <a
+                    href="#"
+                    className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+                  >
+                    Get started
+                  </a>
+                  <a href="#" className="text-sm/6 text-white">
+                    Learn more <span aria-hidden="true">→</span>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
