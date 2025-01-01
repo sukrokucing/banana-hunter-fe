2f1af81948dc4bb82ffcdd3a728fc0b2f6b3819c
jimmy <jimmy@dwp.co.id>
2025-01-01 21:47:21 +0700

    feat(preloader): enhance SVG preloader with new dimensions and animations

--- src/components/ui/preloader-custom.jsx
+++ src/components/ui/preloader-custom.jsx
@@ -5,17 +5,66 @@ const Preloader = () => {
       <div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          className="text-rose-900/60 w-40 h-40"
-          viewBox="0 0 378 378"
+          viewBox="0 0 1500 1500"
+          width="1500"
+          height="1500"
+          preserveAspectRatio="xMidYMid meet"
+          className="w-44 h-44 transition-transform z-50"
+          style={{
+            transform: `translate3d(0px, 0px, 0px)`,
+            contentVisibility: `visible`,
+          }}
+          id="Heart"
         >
-          <g>
-            <path d="M348.151,54.514c-19.883-19.884-46.315-30.826-74.435-30.826c-28.124,0-54.559,10.942-74.449,30.826l-9.798,9.8l-9.798-9.8   c-19.884-19.884-46.325-30.826-74.443-30.826c-28.117,0-54.56,10.942-74.442,30.826c-41.049,41.053-41.049,107.848,0,148.885   l147.09,147.091c2.405,2.414,5.399,3.892,8.527,4.461c1.049,0.207,2.104,0.303,3.161,0.303c4.161,0,8.329-1.587,11.498-4.764   l147.09-147.091C389.203,162.362,389.203,95.567,348.151,54.514z M325.155,180.404L189.47,316.091L53.782,180.404   c-28.368-28.364-28.368-74.514,0-102.893c13.741-13.739,32.017-21.296,51.446-21.296c19.431,0,37.702,7.557,51.438,21.296   l21.305,21.312c6.107,6.098,16.897,6.098,23.003,0l21.297-21.312c13.737-13.739,32.009-21.296,51.446-21.296   c19.431,0,37.701,7.557,51.438,21.296C353.526,105.89,353.526,152.039,325.155,180.404z" />
+          <defs>
+            <clipPath id="__lottie_element_2254">
+              <rect width="1500" height="1500" x="0" y="0" />
+            </clipPath>
+          </defs>
+          <g clipPath="url(#__lottie_element_2254)">
+            <g
+              transform="matrix(1,0,0,1,0,0)"
+              opacity="1"
+              style={{
+                display: `block`,
+              }}
+            >
+              <g opacity="1" transform="matrix(1,0,0,1,1052.260009765625,750)">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fillOpacity="0"
+                  stroke="rgb(212,88,133)"
+                  strokeOpacity="1"
+                  strokeWidth="43"
+                  d=" M-302.260009765625,-212.1280059814453 C-302.260009765625,-386.2049865722656 -166.9340057373047,-527.322998046875 0,-527.322998046875 C166.93299865722656,-527.322998046875 302.260009765625,-386.2049865722656 302.260009765625,-212.1280059814453 C302.260009765625,186.80499267578125 -144.08799743652344,446.12799072265625 -269.7139892578125,511.2139892578125"
+                />
+              </g>
+            </g>
+            <g
+              transform="matrix(1,0,0,1,0,0)"
+              opacity="1"
+              style={{
+                display: `block`,
+              }}
+            >
+              <g opacity="1" transform="matrix(1,0,0,1,447.739990234375,750)">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fillOpacity="0"
+                  stroke="rgb(212,88,133)"
+                  strokeOpacity="1"
+                  strokeWidth="43"
+                  d=" M302.260009765625,-212.1280059814453 C302.260009765625,-386.2049865722656 166.9340057373047,-527.322998046875 0,-527.322998046875 C-166.93299865722656,-527.322998046875 -302.260009765625,-386.2049865722656 -302.260009765625,-212.1280059814453 C-302.260009765625,186.80499267578125 144.08799743652344,446.12799072265625 269.7139892578125,511.2139892578125"
+                />
+              </g>
+            </g>
           </g>
         </svg>
       </div>
 
-      <div className="absolute font-playfair text-center text-xl font-semibold text-white z-40">
+      <div className="absolute animate-tracking-in-expand font-playfair text-center text-xl font-semibold text-white z-40">
         Jimmy
         <br />
         <label className="text-sm text-white">&amp;</label>
